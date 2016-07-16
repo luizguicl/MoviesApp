f6a03e45d55cfa3c409f492218c9c98d9d371568
Luiz Guilherme <luizguicl@gmail.com>
2016-07-16 12:24:05 -0300

    add files

---
+++ app/src/main/java/example/moviesapp/MainActivity.java
@@ -0,0 +1,26 @@
+package example.moviesapp;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements MoviesFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, MoviesFragment.newInstance())
+                    .commit();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
---
+++ app/src/main/java/example/moviesapp/MoviesFragment.java
@@ -0,0 +1,118 @@
+package example.moviesapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MoviesFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MoviesFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MoviesFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public MoviesFragment() {
+        // Required empty public constructor
+    }
+
+    public static MoviesFragment newInstance() {
+        MoviesFragment fragment = new MoviesFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Create some dummy data for the GridView.
+        String[] data = {
+               "X-men Apocalypse",
+                "War Craft",
+                "Ghostbusters",
+                "Finding Dory",
+                "Suicide Squad",
+        };
+        List<String> recentMovies = new ArrayList<String>(Arrays.asList(data));
+
+        ArrayAdapter<String> moviesAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_movie,
+                R.id.name,
+                recentMovies
+        );
+
+        View rootView = inflater.inflate(R.layout.fragment_movies, container, false);
+
+        // Get a reference to the ListView, and attach this adapter to it.
+        GridView listView = (GridView) rootView.findViewById(R.id.moviesList);
+        listView.setAdapter(moviesAdapter);
+
+
+        return rootView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
