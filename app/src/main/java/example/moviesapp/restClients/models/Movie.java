b246549610557d23032dd9bf6903484d451cb066
Luiz Guilherme <luizguicl@gmail.com>
2016-07-18 23:19:44 -0300

    add model files

---
+++ app/src/main/java/example/moviesapp/restClients/models/Movie.java
@@ -0,0 +1,17 @@
+package example.moviesapp.restClients.models;
+
+public class Movie {
+
+    public String posterPath;
+
+    public String overview;
+    public String releaseDate;
+
+    public int id;
+
+    public String title;
+
+    public double voteAverage;
+
+
+}
---
+++ app/src/main/java/example/moviesapp/restClients/models/TheMovieDbResponse.java
@@ -0,0 +1,9 @@
+package example.moviesapp.restClients.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TheMovieDbResponse {
+    public List<Movie> results = new ArrayList<>();
+
+}
